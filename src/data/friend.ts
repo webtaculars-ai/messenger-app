a8316d70cc40fd7d7e0092ae13b19872c5f4e2a5
Abhishek Gupta <webtaculars.ai@gmail.com>
2024-09-12 12:13:34 +0530

    Add friend data

---
+++ src/data/friend.ts
@@ -0,0 +1,17 @@
+const friends = [
+  {
+    id: 1,
+    name: "Sachin",
+    avatar: "https://robohash.org/Alice?set=set4&size=40x40",
+  },
+  {
+    id: 2,
+    name: "Rahul",
+    avatar: "https://robohash.org/Bob?set=set4&size=40x40",
+  },
+  {
+    id: 3,
+    name: "Virat",
+    avatar: "https://robohash.org/Charlie?set=set4&size=40x40",
+  },
+];
